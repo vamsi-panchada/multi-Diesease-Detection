116a05192d4b547d38d2518fae23d9581b4f0b62
vamsi panchada <vamsivs58@gmail.com>
2023-04-05 12:21:34 +0530

    application

---
+++ app.py
@@ -0,0 +1,99 @@
+import streamlit as st
+import numpy as np
+from tensorflow.keras.applications.mobilenet import MobileNet
+from tensorflow.keras.layers import GlobalAveragePooling2D, Dense, Dropout, Flatten
+from tensorflow.keras.models import Sequential
+import cv2
+
+ref = {'Atelectasis': 0.027347086,
+       'Cardiomegaly': 0.010086267,
+       'Consolidation': 0.0018699166,
+       'Edema': 0.0007388931,
+       'Effusion': 0.02946913,
+       'Emphysema': 0.004008924,
+       'Fibrosis': 0.0056709056,
+       'Hernia': 0.00016801473,
+       'Infiltration': 0.13746345,
+       'Mass': 0.00069181505,
+       'Nodule': 0.0048904316,
+       'Normal': 0.38120866,
+       'Pleural_Thickening': 0.0011160322,
+       'Pneumonia': 0.00044845155,
+       'Pneumothorax': 0.0021950703}
+
+labels = ['Atelectasis',
+          'Cardiomegaly', 
+          'Consolidation',
+          'Edema',
+          'Effusion',
+          'Emphysema',
+          'Fibrosis',
+          'Hernia',
+          'Infiltration',
+          'Mass',
+          'Nodule',
+          'Normal',
+          'Pleural_Thickening',
+          'Pneumonia',
+          'Pneumothorax']
+
+base_mobilenet_model = MobileNet(input_shape =  (128, 128, 3), include_top = False, weights = None)
+multi_disease_model = Sequential()
+multi_disease_model.add(base_mobilenet_model)
+multi_disease_model.add(GlobalAveragePooling2D())
+multi_disease_model.add(Dropout(0.5))
+multi_disease_model.add(Dense(512))
+multi_disease_model.add(Dropout(0.5))
+multi_disease_model.add(Dense(len(labels), activation = 'sigmoid'))
+multi_disease_model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['binary_accuracy', 'mae'])
+
+multi_disease_model.load_weights('multiClassMobileNet.hdf5')
+
+st.title('Multiple Disease detection Application')
+
+containerArray = []
+imageArray = []
+betaColumnArray = []
+
+
+uploadedFiles = st.file_uploader('Upload Chest X-Rays', accept_multiple_files=True)
+
+if uploadedFiles:
+    st.markdown("""<hr style="height:10px;border:none;color:#333;background-color:#333;" /> """, unsafe_allow_html=True)
+    for upload_file in uploadedFiles:
+
+        file_bytes = np.asarray(bytearray(upload_file.read()), dtype=np.uint8)
+        im = cv2.imdecode(file_bytes, 1)
+        container = st.container()
+        # container.markdown('<style>body{background-color: Blue;}</style>',unsafe_allow_html=True)
+        col1, col2 = container.columns([230, 230])
+        with col1:
+            col1.image(cv2.resize(im, (224, 224), interpolation=cv2.INTER_CUBIC), channels="BGR")
+        containerArray.append(container)
+        imageArray.append(im)
+        betaColumnArray.append(col2)
+        st.markdown("""<hr style="height:10px;border:none;color:#333;background-color:#333;" /> """, unsafe_allow_html=True)
+
+if len(imageArray)>0:
+    if st.button('Predict'):
+
+        for container, im, col2 in zip(containerArray, imageArray, betaColumnArray):
+
+            im = cv2.resize(im, (128, 128), interpolation=cv2.INTER_CUBIC)
+            im = im.astype(np.float32)/255.
+            im = im.reshape(1, 128, 128, 3)
+
+            res = multi_disease_model.predict(im)
+            
+            normal = True
+            for label, re in zip(labels, res[0]):
+                if label != 'NORMAL':
+                    if ref[label] >= re:
+                        col2.warning(label + ' is positive with : ' +  str(re*100) + ' accuracy ')
+                        normal = False
+                    else:
+                        col2.success(label + ' is negative.')
+            
+            if normal:
+                col2.write("It looks everything is fine")
+
